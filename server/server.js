b0c6125e3a39dc4df792b87f45fd20290dc353e8
Ryan Wiser <wiserryan@gmail.com>
2023-03-16 03:34:07 -0500

    listening on Port

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// Require express
+const express = require('express');
+
+const app = express();
+// Heroku assigns us a unique PORT
+// Use 5001 for localhost development
+const port = process.env.PORT || 5001;
+
+// Look here for files
+app.use(express.static('server'));
+
+// Listen for requests for files
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+});
